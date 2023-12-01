4fc65af5c297a7266f8ce585aa54c9f2249af9d0
Risper <mutindarisper@gmail.com>
2023-12-01 14:14:26 +0300

    add a new task

--- src/components/ComponentA.tsx
+++ src/components/ComponentA.tsx
@@ -1,27 +1,175 @@
 // ComponentA.tsx
 
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import './Tasks.css'
 
 const ComponentA: React.FC = () => {
-  const dataToSend = 'This is the data to be passed';
+
+
   const navigate = useNavigate();
+
+  const dataToSend = 'This is the data to be passed';
+
   const handleClick = () => {
-    navigate('/componentB', { state: { data: dataToSend } });
+    navigate(`/componentB/${dataToSend}`, { state: { data: dataToSend } });
   };
+
+  const [todo, setTodo] = useState([
+    { id: 1, text: 'Learn React', status: false },
+    { id: 2, text: 'Build a project', status: false },
+    { id: 3, text: 'Practice TypeScript', status: false }
+  ])
+
+  const [newTask, setNewTask] = useState('')
+  const [updateData, setUpdateData] = useState('')
+
+  //add new tasks
+  const addTask = () => {
+    if(newTask) {
+      let new_taskId = todo.length + 1
+      let newTaskEntry =  {id: new_taskId, text: newTask, status: false}
+      setTodo([...todo, newTaskEntry])
+      setNewTask('')
+    }
+
+  }
+  //add new tasks
+  const deleteTask = () => {
+
+  }
+
+  //add new tasks
+  const markTaskDone = () => {
+
+  }
+
+  //add new tasks
+  const cancelUpdate = () => {
+
+  }
+
+  //add new tasks
+  const changeTask = () => {
+
+  }
+
+  //add new tasks
+  const updateTask = () => {
+
+  }
+
+
+
+
+
 
   return (
     <div>
-      <h1>Component A</h1>
+      {/* <h1>Component A</h1> */}
       {/* Using Link */}
-      <Link to={`/componentB/${encodeURIComponent(dataToSend)}`}>
+      {/* <Link to={`/componentB/${encodeURIComponent(dataToSend)}`}>
         Go to Component B
-      </Link>
+      </Link> */}
 
-      <div>
+
       <h1>Component A</h1>
       <button onClick={handleClick}>Go to Component B</button>
-    </div>
+
+
+      <div className="container App">
+
+
+         {/* {update tasks} */}
+         <div className="row">
+        <div className="col">
+          <input 
+            value={ updateData && updateData }
+            onChange={ (e) => changeTask()}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button
+            onClick={updateTask}
+            className="btn btn-lg btn-success mr-20"
+          >Update</button>
+          <button
+            onClick={cancelUpdate}
+            className="btn btn-lg btn-warning"
+          >Cancel</button>
+        </div>
+      </div>
+      <br />
+
+
+
+
+
+      {/* {Add new tasks} */}
+      <div className="row">
+        <div className="col">
+          <input 
+            value={newTask}
+            onChange={ (e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button
+            onClick={addTask}
+            className="btn btn-lg btn-success"
+          >Add Task</button>
+        </div>
+      </div>
+      <br />
+
+
+
+
+        {
+          todo && todo.length ? '' : 'No Tasks Available'
+        }
+
+
+        {
+          todo && todo.map((task, index) => {
+            return (
+              <div key={task.id}>
+
+
+                <div className='col taskBg'>
+                  <div className={task.status ? 'done' : ''}>
+                    <span className='taskNumber'>{index + 1}</span>
+                    <span className='taskText'>{task.text}</span>
+
+                  </div>
+
+                  <div className="iconsWrap">
+                    <span >
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span >
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+
+                  </div>
+
+                </div>
+              </div>
+            )
+          })
+        }
+
+      </div>
+
 
     </div>
   );
--- src/components/ComponentB.tsx
+++ src/components/ComponentB.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import {  useLocation } from 'react-router-dom';
+// Define a type for the received data         useParams,
+type ReceivedDataType = {
+  data: string;
+};
 
 const ComponentB: React.FC = () => {
+
     const location = useLocation();
-    const receivedData = location.state?.data;
-  const { data } = useParams<{ data?: string }>();
+
+    const receivedData = (location.state as ReceivedDataType | undefined )?.data;
+    
+    console.log(receivedData)
+  // const { data } = useParams<{ data?: string }>();
 
   return (
     <div>
       <h1>Component B</h1>
-      <p>Data received: {data ? decodeURIComponent(data) : 'No data available'}</p>
+      {/* <p>Data received: {data ? decodeURIComponent(data) : 'No data available'}</p> */}
       <p>Data received: {receivedData ? decodeURIComponent(receivedData) : 'No data available'}</p>
     </div>
   );
